4fa187d51df261237f6e890ee980e10676655908
Alif <alifaziz@gmail.com>
2023-01-11 23:26:49 +0800

    Add web app

--- src/main/java/com/filavents/books_highlights/MainVerticle.java
+++ src/main/java/com/filavents/books_highlights/MainVerticle.java
@@ -9,6 +9,7 @@ import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 import java.util.concurrent.TimeUnit;
 
@@ -49,13 +50,13 @@ public class MainVerticle {
     router.get("/api/books/:bookId").respond(NoteController::getNotesByBookId);
 
     // setindexpage return index.html page
-    //router.get("/").handler(ctx -> ctx.response().sendFile("web/reddit-ama-web/dist/reddit-ama-web/index.html"));
+    router.get("/").handler(ctx -> ctx.response().sendFile("web/playbooks/dist/playbooks/index.html"));
 
     // Frontend router path
-    //router.get("/view/*").handler(ctx -> ctx.response().sendFile("web/reddit-ama-web/dist/reddit-ama-web/index.html"));
+    router.get("/page/*").handler(ctx -> ctx.response().sendFile("web/playbooks/dist/playbooks/index.html"));
 
     // Set static web root
-    //router.route("/*").handler(StaticHandler.create().setWebRoot("web/reddit-ama-web/dist/reddit-ama-web"));
+    router.route("/*").handler(StaticHandler.create().setWebRoot("web/playbooks/dist/playbooks"));
 
     // Start the server
     int runningPort = Integer.parseInt(System.getenv("PORT"));
