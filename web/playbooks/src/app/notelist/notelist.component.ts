889160f6c696407cf65841e587cf93bc6a9ce102
Alif <alifaziz@gmail.com>
2023-01-27 15:20:59 +0800

    Add anchor tag

--- web/playbooks/src/app/notelist/notelist.component.ts
+++ web/playbooks/src/app/notelist/notelist.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { Location } from '@angular/common';
 import { BookService } from '../book.service';
 import { Notes } from '../Notes';
 
@@ -14,8 +15,9 @@ export class NotelistComponent {
   constructor(
     private bookService: BookService,
     private route: ActivatedRoute,
-    private titleService: Title
-    ) { }
+    private titleService: Title,
+    private location: Location
+  ) { }
 
   notes: Notes = {
     data: [],
@@ -27,14 +29,28 @@ export class NotelistComponent {
   bookId: string = '1';
 
   ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        setTimeout(() => {
+          const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+        }, 1000);
+      }
+    });
+
     this.route.params.subscribe(params => {
       this.bookId = params['bookId'];
       this.getNotesByBookId(this.bookId);
     });
-
   }
 
-  getNotesByBookId(bookId: string): void {
+  getFragmentUrl(noteId: number): string {
+    return this.location.path() + '#' + noteId;
+  }
+
+  private getNotesByBookId(bookId: string): void {
     this.bookService.getBook(bookId).subscribe(data => {
       this.notes = data;
       this.hasLoaded = true;
